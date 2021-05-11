fae60e3942224327fb60a01da0bd8c0f706a25a1
Red-Spheal <magiccubeur@gmail.com>
2021-05-11 13:12:36 +0200

    Adding calendar in separate tab in TabLayout

--- app/src/main/java/com/example/bingoetagelta/MainActivity.kt
+++ app/src/main/java/com/example/bingoetagelta/MainActivity.kt
@@ -6,7 +6,15 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
 import androidx.preference.PreferenceManager
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(){
@@ -15,6 +23,22 @@ class MainActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar_main))
+
+        // ViewPager
+        val viewPager = findViewById<ViewPager2>(R.id.view_pager_main)
+        val viewPagerAdapter = ViewPagerFragmentAdapter(supportFragmentManager, lifecycle)
+        viewPager.adapter = viewPagerAdapter
+
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout_main)
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when(position){
+                0 -> resources.getString(R.string.first_tab_name)
+                1 -> resources.getString(R.string.second_tab_name)
+                else -> resources.getString(R.string.unknown_tab)
+            }
+            //viewPager.setCurrentItem(tab.position, true)
+        }.attach()
 
         usernameCheck()
     }
@@ -53,4 +77,25 @@ class MainActivity : AppCompatActivity(){
         }
     }
 
+    class ViewPagerFragmentAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+        FragmentStateAdapter(fragmentManager, lifecycle) {
+
+        private val fragmentArray = arrayOf<Fragment>( //Initialize fragments views
+            BingoFragment(),
+            CalendarFragment.newInstance(
+                Calendar.getInstance().get(Calendar.DAY_OF_YEAR),
+                Calendar.getInstance().get(Calendar.MONTH),
+                Calendar.getInstance().get(Calendar.YEAR)
+            ),
+        )
+
+
+        override fun getItemCount(): Int {
+            return fragmentArray.size
+        }
+
+        override fun createFragment(position: Int): Fragment {
+            return fragmentArray[position]
+        }
+    }
 }

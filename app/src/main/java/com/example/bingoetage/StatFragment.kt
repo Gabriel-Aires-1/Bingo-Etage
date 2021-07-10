c9416611bbf65ec4e12b5d24a324d1cd8993e07f
Red-Spheal <magiccubeur@gmail.com>
2021-07-10 20:32:43 +0200

    Changing charts to AnyChart library

--- app/src/main/java/com/example/bingoetage/StatFragment.kt
+++ app/src/main/java/com/example/bingoetage/StatFragment.kt
@@ -6,14 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import com.anychart.APIlib
+import com.anychart.AnyChart
+import com.anychart.AnyChartView
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.anychart.charts.Cartesian
+import com.anychart.data.Set
 import com.example.bingoetage.databinding.FragmentStatBinding
 import com.example.bingoetage.viewmodel.BingoGrid
 import com.example.bingoetage.viewmodel.BingoViewModel
-import com.jjoe64.graphview.DefaultLabelFormatter
-import com.jjoe64.graphview.GraphView
-import com.jjoe64.graphview.GridLabelRenderer
-import com.jjoe64.graphview.series.BarGraphSeries
-import com.jjoe64.graphview.series.DataPoint
 import dagger.hilt.android.AndroidEntryPoint
 import java.util.*
 
@@ -31,7 +33,7 @@ class StatFragment : Fragment() {
     private var _binding: FragmentStatBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var graphAveragePerMonths: GraphView
+    private lateinit var graphAveragePerMonths: AnyChartView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,53 +47,44 @@ class StatFragment : Fragment() {
         _binding = FragmentStatBinding.inflate(inflater, container, false)
 
         graphAveragePerMonths = binding.graphAveragePerMonths
-        graphAveragePerMonths.title = resources.getString(R.string.average_graph_title)
-        graphAveragePerMonths.viewport.isScrollable = true
-        graphAveragePerMonths.viewport.isXAxisBoundsManual = true
-        graphAveragePerMonths.viewport.setMinX(202100.0)
-        graphAveragePerMonths.viewport.setMaxX(202105.0)
-        graphAveragePerMonths.gridLabelRenderer.horizontalAxisTitle = resources.getString(R.string.average_graph_x_title)
-        graphAveragePerMonths.gridLabelRenderer.verticalAxisTitle = resources.getString(R.string.average_graph_y_title)
-        graphAveragePerMonths.gridLabelRenderer.gridStyle = GridLabelRenderer.GridStyle.BOTH
-        graphAveragePerMonths.gridLabelRenderer.numHorizontalLabels = 6
+        graphAveragePerMonths.setProgressBar(binding.progressBar)
 
-        graphAveragePerMonths.gridLabelRenderer.labelFormatter = object : DefaultLabelFormatter() {
-            override fun formatLabel(value: Double, isValueX: Boolean): String {
-                return if (isValueX) {
-                    // X values month and year
-                    val year = value.toInt()/100
-                    val month = value.toInt() % 100
+        val vertical: Cartesian = AnyChart.vertical()
 
-                    val calFmt = Calendar.getInstance()
-                    calFmt.set(Calendar.MONTH, month)
-                    calFmt.set(Calendar.YEAR, year)
+        vertical.animation(true)
+            .title(resources.getString(R.string.average_graph_title))
 
-                    resources.getString(R.string.average_graph_x_label_format)
-                        .format(
-                            //((value.toInt() + 1) % 100).toString(),
-                            String.format("%1\$tb", calFmt)
-                                .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() },
-                            (value.toInt()/100).toString()
-                        )
-                } else {
-                    // y values converted to Int
-                    value.toInt().toString()
-                }
-            }
-        }
+        val set = Set.instantiate()
+        val barData = set.mapAs("{ x: 'x', value: 'value' }")
+        val bar = vertical.bar(barData)
+        bar.labels().format("{%Value}")
+
+        vertical.yScale().minimum(0.0)
+        vertical.labels(true)
+            .xAxis(true)
+            .yAxis(true)
+
+        vertical.xScroller(true)
+
+        vertical.xZoom().setToPointsCount(6, false, vertical.xScale())
+        vertical.xScroller().allowRangeChange(false)
+            .thumbs(false)
+
+
+        graphAveragePerMonths.setChart(vertical)
 
         // Setup observer for month average graph view
         viewModel.getEditingBingoGrids(false).observe(
             viewLifecycleOwner,
-            { bingoGridList -> updateGraphAPM(bingoGridList) }
+            { bingoGridList -> bar.data(getListForGraphAPM( bingoGridList )) }
         )
 
         return binding.root
     }
 
-    private fun updateGraphAPM(bingoGridList: List<BingoGrid>?)
-    {
+    private fun getListForGraphAPM(bingoGridList: List<BingoGrid>?): MutableList<DataEntry> {
         val sumResultAndCountPerMonths = mutableMapOf<Pair<Int, Int>, Pair<Int, Int>>()
+
         bingoGridList?.forEach { bingoGrid ->
             val yearMonthPair = Pair<Int, Int>(bingoGrid.year, bingoGrid.month)
             sumResultAndCountPerMonths[yearMonthPair] = Pair(
@@ -105,35 +98,41 @@ class StatFragment : Fragment() {
             averagePerMonths[yearMonth] = sumCount.first / sumCount.second.toDouble()
         }
 
-        val datapoints = mutableListOf<DataPoint>()
+        val sortedAveragePerMonths = averagePerMonths.toSortedMap(compareBy({it.first}, {it.second}))
+        val firstData = sortedAveragePerMonths.firstKey()
+        val lastData = sortedAveragePerMonths.lastKey()
+        for (i in 0..((lastData.first-firstData.first)*12+(lastData.second-firstData.second)))
+        {
+            val key = Pair(firstData.first+(firstData.second+i)/12, (firstData.second+i)%12)
+            if(!sortedAveragePerMonths.containsKey(key))
+                sortedAveragePerMonths[key] = 0.0
+        }
 
-        averagePerMonths
-            .toSortedMap(compareBy({it.first}, {it.second}))
+
+        val dataEntries = mutableListOf<DataEntry>()
+        val calFmt = Calendar.getInstance()
+
+        sortedAveragePerMonths
             .forEach { (yearMonth, average) ->
-                datapoints.add(
-                    DataPoint(
-                        yearMonth.first*100+yearMonth.second.toDouble(),
-                        average
+                calFmt.set(Calendar.MONTH, yearMonth.second)
+                calFmt.set(Calendar.YEAR, yearMonth.first)
+
+                dataEntries.add(
+                    ValueDataEntry(
+                        String.format("%1\$tb", calFmt)
+                            .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() },
+                        average,
                     )
                 )
             }
-
-        val series = BarGraphSeries(
-            datapoints.toTypedArray()
-        )
-
-        graphAveragePerMonths.addSeries(series)
-        graphAveragePerMonths.viewport.setMinY(0.0)
-        graphAveragePerMonths.viewport.isYAxisBoundsManual = true
-        series.isDrawValuesOnTop = true
-        series.valuesOnTopColor = graphAveragePerMonths.titleColor
-        series.spacing = 50
+        return dataEntries
     }
 
     override fun onDestroyView()
     {
         super.onDestroyView()
         _binding = null
+        APIlib.getInstance().setActiveAnyChartView(null)
     }
 
     companion object {

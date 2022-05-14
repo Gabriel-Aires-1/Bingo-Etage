7b83afa327902e634633e4853efaf611d93630f9
Gabriel AIRES <93598786+Gabriel-Aires-1@users.noreply.github.com>
2022-05-14 19:10:30 +0200

    Ajout d'un sélecteur d'année pour le fragment FloorPieChartFragment.kt

--- app/src/main/java/com/example/bingoetage/statistictabs/FloorPieChartFragment.kt
+++ app/src/main/java/com/example/bingoetage/statistictabs/FloorPieChartFragment.kt
@@ -5,9 +5,13 @@ import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.annotation.ColorInt
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LiveData
 import com.example.bingoetage.R
 import com.example.bingoetage.databinding.FragmentFloorPieChartBinding
 import com.example.bingoetage.viewmodel.BingoGrid
@@ -20,6 +24,7 @@ import com.github.mikephil.charting.data.PieEntry
 import com.github.mikephil.charting.formatter.ValueFormatter
 import com.github.mikephil.charting.utils.ColorTemplate
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 import kotlin.collections.ArrayList
 
 
@@ -29,15 +34,19 @@ import kotlin.collections.ArrayList
  * create an instance of this fragment.
  */
 @AndroidEntryPoint
-class FloorPieChartFragment : Fragment() {
+class FloorPieChartFragment : Fragment(), AdapterView.OnItemSelectedListener {
 
     private val viewModel: BingoViewModel by activityViewModels()
 
     private var _binding: FragmentFloorPieChartBinding? = null
     private val binding get() = _binding!!
+    private var _yearSpinner: Spinner? = null
+    private val yearSpinner get() = _yearSpinner!!
 
     private var _floorPieChart: PieChart? = null
     private val floorPieChart get() = _floorPieChart!!
+
+    private var bingoGridList: LiveData<List<BingoGrid>>? = null
 
     @ColorInt private var textColor = 0
 
@@ -66,13 +75,61 @@ class FloorPieChartFragment : Fragment() {
         floorPieChart.setEntryLabelColor(textColor)
         floorPieChart.setEntryLabelTextSize(20f)
 
-        viewModel.getEditingBingoGrids(false).observe(
+        setYearSpinnerSettings()
+
+        return binding.root
+    }
+
+    private fun setYearSpinnerSettings()
+    {
+        // Year spinner
+        // On year list data update, change the year list in the spinner
+        // On selection change, change the livedata observed for graph update
+        _yearSpinner = binding.yearSpinner
+        yearSpinner.onItemSelectedListener = this
+        viewModel.getDistinctYears().observe(
+            viewLifecycleOwner
+        ) { yearList ->
+
+            val valueList = yearList.map { it.toString() }.toMutableList()
+            val currentYear = viewModel.currentDate.value?.get(Calendar.YEAR)?.toString()
+
+            valueList.add(0, resources.getString(R.string.year_spinner_all_option))
+
+            if (valueList.count() == 1)
+                currentYear?.let { valueList.add(it) }
+
+            val previouslySelectedValue =
+                yearSpinner.selectedItem?.toString() ?: currentYear
+
+            val valueToSelect = (if (valueList.contains(previouslySelectedValue)) previouslySelectedValue else currentYear) ?: valueList[0]
+
+            val adapter =
+                ArrayAdapter(
+                    requireContext(),
+                    android.R.layout.simple_spinner_item,
+                    valueList
+                )
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+            yearSpinner.adapter = adapter
+            yearSpinner.setSelection(valueList.indexOf(valueToSelect))
+        }
+    }
+
+    // Observe the livedata corresponding to the year and update the chart
+    private fun updatePieChartValues(allYears: Boolean, year: Int?=null)
+    {
+        bingoGridList?.removeObservers(viewLifecycleOwner)
+
+        if (allYears) bingoGridList = viewModel.getEditingBingoGrids(false)
+        else if (year != null) bingoGridList = viewModel.getYearEditingBingoGrids(year, false)
+
+        bingoGridList?.observe(
             viewLifecycleOwner
         ) { bingoGridList ->
             updatePieChartDisplay(getListForFPC(bingoGridList))
         }
-
-        return binding.root
     }
 
     // Transform the data to a list usable by the chart
@@ -151,6 +208,7 @@ class FloorPieChartFragment : Fragment() {
         super.onDestroyView()
         _binding = null
         _floorPieChart = null
+        _yearSpinner = null
     }
 
     companion object {
@@ -163,4 +221,16 @@ class FloorPieChartFragment : Fragment() {
         @JvmStatic
         fun newInstance() = FloorPieChartFragment()
     }
+
+    // Spinner selection logic
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        when(parent?.id)
+        {
+            R.id.yearSpinner ->
+                if (position == 0) updatePieChartValues(true)
+                else updatePieChartValues(false, parent.getItemAtPosition(position).toString().toInt())
+        }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
 }
